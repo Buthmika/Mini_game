45f05a149f84517483d8a1b1dfb19f827f9d28f9
Buthmika <buthmikamenushan@gmail.com>
2025-03-29 20:11:22 +0530

    Update CatchTheFallingObjects.py

--- CatchTheFallingObjects.py
+++ CatchTheFallingObjects.py
@@ -13,3 +13,19 @@ pygame.display.set_caption("Catch the Falling Objects")
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+# Basket properties
+basket_width = 80
+basket_height = 20
+basket_x = WIDTH // 2 - basket_width // 2
+basket_y = HEIGHT - 50
+basket_speed = 7
+
+# Apple properties
+apple_radius = 15
+apple_x = random.randint(apple_radius, WIDTH - apple_radius)
+apple_y = 0
+apple_speed = 5
+
+# Score
+score = 0
+font = pygame.font.Font(None, 36)
