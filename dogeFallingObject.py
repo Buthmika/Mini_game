a213559ec18ac7ac141280d9c5154677aa6c54e8
Buthmika <buthmikamenushan@gmail.com>
2025-03-31 21:42:58 +0530

    Update dogeFallingObject.py

--- dogeFallingObject.py
+++ dogeFallingObject.py
@@ -1,2 +1,9 @@
 import pygame
 import random
+# Initialize Pygame
+pygame.init()
+
+# Screen settings
+WIDTH, HEIGHT = 500, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Dodge the Falling Objects")
